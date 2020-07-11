3ef69908f93ad2f8c990fd5b8ed042608906804d
masahitojp <randomstep@gmail.com>
2020-07-12 01:24:22 +0900

    chore: :sparkles: add new contents / memos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,25 @@ import Typography from '@material-ui/core/Typography'
 
 const HomePage: React.SFC = () => (
   <Base>
+    <Typography variant="h5" align="center" component="h2">
+      Memos
+    </Typography>
+    <Typography
+      variant="subtitle1"
+      align="center"
+      color="textSecondary"
+      component="p"
+    >
+      <Typography component="h3" variant="h6" color="inherit" align="center">
+        <a
+          href="https://gist.github.com/masahitojp/bad7db7c418b047ae67c4edf9b857f85"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          masahitoが課金しているwebサービス
+        </a>
+      </Typography>
+    </Typography>
     <Typography component="h2" variant="h5" color="inherit" align="center">
       Software
     </Typography>
